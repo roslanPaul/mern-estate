fe57712f18a825420aa9fa3a2b2fc4e10c4408ba
roslanPaul <roslan.p.h.nzamba.nzamba@gmail.com>
2024-02-26 20:13:02 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
 
+try {
+    dotenv.config();
+    console.log(process.env); // Log all loaded environment variables
+  } catch (error) {
+    console.error("Error loading environment variables:", error);
+  }
+  
+const uri = process.env.MONGO;
 const app = express();
+
+console.log(uri);
+
+
+mongoose.connect(uri).then(() => {
+    console.log('MongoDB connected');
+}).catch((err) => {
+    console.log(err);
+    
+});
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
