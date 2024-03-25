235c4cb8decef6596f15fe2993ab31f08b5a45f7
roslanPaul <roslan.p.h.nzamba.nzamba@gmail.com>
2024-03-25 12:26:22 +0000

    connect to new database

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password : hashedPassword});
-    //this saving take time so to prevent an error we use await and make the function async
+    //this saving take time so to prevent an error so we use await and make the function async
     try {
         await newUser.save();
         res.status(201).json({message: 'User created'});
 
     } catch (error) {
-        res.status(500).json({message: error.message});
+        next(error);
     }
 
 };
